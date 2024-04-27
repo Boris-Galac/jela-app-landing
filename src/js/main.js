5859c1c7c00d9eeeea75bddd8622ec4350566b1d
unknown <boris.galac@gmail.com>
2024-04-27 20:53:19 +0200

    asdasd

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,56 @@ hamBtn.addEventListener("click", (e) => {
     hamBtn.setAttribute("data-active", "false");
   }
 });
+
+//// SCROLL TO TOP
+
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 100) {
+    document.querySelector(".nav__logo").setAttribute("data-visible", "true");
+    document.querySelector(".nav").classList.add("nav-active");
+  } else {
+    document.querySelector(".nav__logo").setAttribute("data-visible", "false");
+    document.querySelector(".nav").classList.remove("nav-active");
+  }
+  const backToTopBtn = document.querySelector(".scroll-to-top-btn");
+  if (window.scrollY > 300) {
+    backToTopBtn.setAttribute("data-visible", "true");
+  } else {
+    backToTopBtn.setAttribute("data-visible", "false");
+  }
+  backToTopBtn.addEventListener("click", (e) => {
+    window.scrollTo({ top: 0 });
+  });
+});
+
+// HEADER HIDE ON SCROLL
+
+/////////// HIDE HEADER ON SCROLL
+
+let lastScrollTop = 0;
+let navbar = document.querySelector(".header");
+let navbarHeight = document.querySelector(".header").scrollHeight;
+window.addEventListener("scroll", (e) => {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = `-${navbarHeight}px`;
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
+
+//// BACK TO TOP
+
+const backToTopbtn = document.querySelector(".back-to-top-btn");
+
+backToTopbtn.addEventListener("click", (e) => {
+  window.scroll({
+    top: 0,
+  });
+});
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 150) {
+    backToTopbtn.setAttribute("data-visible", "true");
+  } else backToTopbtn.setAttribute("data-visible", "false");
+});
